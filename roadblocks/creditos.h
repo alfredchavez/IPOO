f1999f87a9ddf93319f737a36c962a061a33bd68
alfredchavez <alfred.chavez@ucsp.edu.pe>
2015-09-17 09:09:08 -0500

    Create creditos.h

---
+++ roadblocks/creditos.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void creditos(){
+  cout<<"Autores :"<<endl;
+  cout<<"Alfred Chavez Urday"<<endl;
+  cout<<"Rodrigo Sanchez Pinto"<<endl;
+  cout<<endl;
+  return;
+}
