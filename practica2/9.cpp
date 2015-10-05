57be50516a6c6f52ba2f7cdb99c388114fd10711
alfredchavez <alfred.chavez@ucsp.edu.pe>
2015-10-05 15:22:22 -0500

    incomplete

---
+++ practica2/9.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int entero(char a){
+   if(a<96)return a-65;
+   return a-97;
+}
+
+int main(){
+   string s;
+   getline(cin,s);
+   cout<<"Cuantas letras tiene su nombre?"<<endl;
+   int n;
+   cin>>n;
+   for(int i=0;i<s.length()-n;i++){
+      for(int j=0;j<n;j++){
+	 if()
+      }
+   }
+}
